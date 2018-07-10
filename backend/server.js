717eebfd5f2e9c7b8fe89f03088a3c43d661a69c
jjocqu <jens@jocque.be>
2018-07-10 16:02:34 +0200

    frontend setup

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,15 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'html');
 
 app.use([bodyParser.json(), bodyParser.urlencoded({extended: true})]);
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send('Server error');
 });
 app.use('/api', routes);
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+app.use('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 
